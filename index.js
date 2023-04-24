7bc523cea9775e384ae709512966f6e2332544c8
Fatima Ali <fofoalmali@gmail.com>
2023-04-24 17:38:20 +0300

    assignment solved

--- index.js
+++ index.js
@@ -10,5 +10,16 @@ class Formatter {
     return str;
   }
 
+  static titleize(str){
+    const exceptLitter=['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const arr= str.split(/\s+/)
+      for(let i=0 ; i< arr.length ;i++){
+        if(!exceptLitter.includes(arr[i])){
+          arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
+        }
+        arr[0]=arr[0].charAt(0).toUpperCase()+arr[0].slice(1);
+       }
+     return arr.join(' ');
+   }
  
 }
